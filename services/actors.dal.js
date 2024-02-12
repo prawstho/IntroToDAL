f9f09f2cb5587a052559fa1ced38ba58dc9ca749
Peter <113168584+prawstho@users.noreply.github.com>
2024-02-12 10:33:25 -0330

    fetched a single actor from db

--- services/actors.dal.js
+++ services/actors.dal.js
@@ -22,6 +22,29 @@ var getActors = function() {
   }); 
 };
 
+var getActorById = function(theId) {
+  if(DEBUG) console.log("actors.dal.getActorById()");
+  console.log(`the actors id is: ${theId}`)
+  return new Promise(function(resolve, reject) {
+
+    const sql = "SELECT actor_id, first_name, last_name FROM actor \
+      WHERE actor_id = $1;"
+
+    dal.query(sql, [theId], (err, result) => {
+      if (err) {
+        // logging should go here
+        if(DEBUG) console.log(err);
+        reject(err);
+      } else {
+        if(DEBUG) console.log("inside the actors.dal.getActorById() function");
+        if(DEBUG) console.log(result.rows);
+        resolve(result.rows);
+      }
+    }); 
+  }); 
+};
+
 module.exports = {
   getActors,
+  getActorById,
 }
